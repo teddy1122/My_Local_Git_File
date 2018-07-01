8f022b53a91526e1b3bb687f02397dcf195e187d
Luan <luanjunfeng11@163.com>
2018-07-01 10:23:59 +0800

    wrote a readme file

---
+++ test.py
@@ -0,0 +1,23 @@
+#hello Python
+print "hello"
+name = 2
+if name>18:
+    print "adult"
+elif name>6:
+    print "teenager"
+else:
+    print "haha"
+print ("name:",name)
+
+sum = 0
+for x in range(101):
+    sum = sum+x 
+print sum
+
+sum1 = 0
+n=99 
+while n>0:
+    sum1 = sum1 + n
+    n = n - 1
+print sum1
+test = raw_input()
---
+++ test2.py
@@ -0,0 +1,11 @@
+#hello Python
+def my_abs(x):
+	if x >=0:
+		return x 
+	else:
+		return -x 
+a=int(raw_input())
+v=my_abs(a)
+print v
+print a 
+raw_input()
